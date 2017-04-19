816f8d125dfd0e4a07fd90982c2d5308416bcf31
Geoffrey Kip <g_kip@live.com>
2017-04-18 21:51:57 -0400

    Add files via upload

--- SVG Elements D3/shapes.js
+++ SVG Elements D3/shapes.js
@@ -10,3 +10,32 @@ svg.selectAll("rect")
                 .attr("fill","pink")
                 .attr("x",function(d,i){ return 60*i; })
                 .attr("y",function(d,i){ return 300-(d*15); });
+
+var newX = 300;
+svg.selectAll("circle.first")
+      .data(dataArray)
+      .enter().append("circle")
+                .attr("class","first")
+                .attr("cx",function(d,i){ newX+=(d*3)+(i*20); return newX; })
+                .attr("cy","100")
+                .attr("r",function(d){ return d*3; });
+
+var newX = 600;
+svg.selectAll("ellipse")
+      .data(dataArray)
+      .enter().append("ellipse")
+                .attr("class","second")
+                .attr("cx",function(d,i){ newX+=(d*3)+(i*20); return newX; })
+                .attr("cy","100")
+                .attr("rx",function(d){ return d*3; })
+                .attr("ry","30");
+
+var newX = 900;
+svg.selectAll("line")
+      .data(dataArray)
+      .enter().append("line")
+                .attr("x1",newX)
+                .attr("stroke-width","2")
+                .attr("y1",function(d,i){ return 80+(i*20); })
+                .attr("x2",function(d){ return newX+(d*15); })
+                .attr("y2",function(d,i){ return 80+(i*20); });
